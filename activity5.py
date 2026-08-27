87c7fa810a880064e5bebef3c3b79a26b7890329
vjethrojoe-cyber <vjethrojoe@gmail.com>
2026-08-27 23:46:31 +0800

    Create activity5.py

---
+++ activity5.py
@@ -0,0 +1,22 @@
+#activity5.py
+
+#operators activity
+
+a = 17
+b = 15
+sum = a + b
+diff = a - b
+prod = a * b
+quot = a / b
+mod = a % b
+exp = a ** b
+floor_div = a // b
+
+
+print("The sum of ", a, " minus ", b, " is ", sum)
+print("The difference of ", a, " minus ", b, " is ", diff)
+print("The product of ", a, " multiplied by ", b, " is ", prod)
+print("The quotient of ", a, " divided by ", " is ", quot)
+print("The remainder of ", a, "divided by ", b, " is ", mod)
+print("The exponent of ", a, " raised to the power of ", b, " is ", exp)
+print("The floor division of ", a, " divided by ", b, " is ", floor_div)
