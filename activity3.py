3c64a9cf419754910cf4b7c6ea98708ccc273373
vjethrojoe-cyber <vjethrojoe@gmail.com>
2026-08-27 22:25:59 +0800

    Create activity3.py

---
+++ activity3.py
@@ -0,0 +1,44 @@
+#activity3.py
+#using variables
+
+#int
+age = 18
+year = 2026
+score = 95
+items_count = 10
+
+#float
+price = 99.99
+height_in_meters = 1.75
+pi = 3.1416
+gpa = 1.25
+
+#string
+course_code = "ITCS102"
+username = "QWERTY"
+message = "Hello, World"
+language = "Python"
+
+#boolean
+is_Handsome = True
+is_code_working = False
+is_spiderman = True
+is_Happy = True
+
+
+print("Age:", age)
+print("Year:", year)
+print("Score:", score)
+print("Items Count:", items_count)
+print("Price:", price)
+print("Height in Meters:", height_in_meters)
+print("Pi:", pi)
+print("GPA:", gpa)
+print("Course Code:", course_code)
+print("Username:", username)
+print("Message:", message)
+print("Language:", language)
+print("Is Handsome:", is_Handsome)
+print("Is Code Working:", is_code_working)
+print("Is Spiderman:", is_spiderman)
+print("Is Happy:", is_Happy)
