76a8c651bf6c41ee6d5f9ec78045b28acb6189af
vjethrojoe-cyber <vjethrojoe@gmail.com>
2026-08-31 22:17:36 +0800

    Create codechallenge2.py

---
+++ codechallenge2.py
@@ -0,0 +1,29 @@
+bal = 6767
+print("balance: ", bal)
+
+comp_1000 = bal//1000
+balance = bal%1000
+comp_500 = bal//500
+balance = bal%500
+comp_200 = bal//200
+balance = bal%200
+comp_100 = bal//100
+balance = bal%100
+comp_50 = bal//50
+balance = bal%50
+comp_20 = bal//20
+balance = bal%20
+comp_10 = bal//10
+balance = bal%10
+comp_5 = bal//5
+balance = bal%5
+
+
+print("1000 =", comp_1000)
+print("500 =", comp_500)
+print("200 =", comp_200)
+print("100 =", comp_100)
+print("50 =", comp_50)
+print("20 =", comp_20)
+print("10 =", comp_10)
+print("5 =", comp_5)
