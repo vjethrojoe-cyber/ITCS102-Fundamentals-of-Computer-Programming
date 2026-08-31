cc4ecb34837773cada3a60638ce00a900ecedb74
vjethrojoe-cyber <vjethrojoe@gmail.com>
2026-08-31 21:56:18 +0800

    Create codechallenge1.py

---
+++ codechallenge1.py
@@ -0,0 +1,10 @@
+#escpae sequence --> to modify the behavior of linear formatted string
+# \n - new line
+# \t - tab space
+# \r - carriage return / deprecated (obsolete)
+# \b - back space
+# \\ - insert slash
+# \" - insert double quotation
+
+
+print("\t\t\t*\n\t\t*\t\t*\n\t*\t\t\t\t*\n*\t\t\t\t\t\t*\n\n*\t\t\t\t\t\t*\n\n*\t\t\t\t\t\t*\n\n*\t\tJethro_Vedra\t\t\t*\n\n*\t\t\t\t\t\t*\n\n*\t\t\t\t\t\t*\n\n*\t\t\t\t\t\t*\n\n*\t*\t*\t*\t*\t*\t*")
