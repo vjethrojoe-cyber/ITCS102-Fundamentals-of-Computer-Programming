0d77a86621cd3eae12924357be5e5b7587ac3e71
vjethrojoe-cyber <vjethrojoe@gmail.com>
2026-09-07 00:29:24 +0800

    Create activity8.py

---
+++ activity8.py
@@ -0,0 +1,15 @@
+food = "" #empty string
+
+f = input("What is your favorite food --> ")
+food += f + ", "
+
+f = input("What is your favorite food --> ")
+food += f + ", "
+
+f = input("What is your favorite food --> ")
+food += f + ", "
+
+f = input("What is your favorite food --> ")
+food += f + ", "
+
+print("My favorite foods are {", food, "}")
