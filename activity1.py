e1d64b7741e924636654af805c84e4f055ad66b1
vjethrojoe-cyber <vjethrojoe@gmail.com>
2026-09-06 15:28:18 +0800

    Create activity1.py

---
+++ activity1.py
@@ -0,0 +1,24 @@
+print("Vedra, Jethro Joe A.")
+print("Vedra, Jethro Joe A.")
+print("Vedra, Jethro Joe A.")
+print("Vedra, Jethro Joe A.")
+print("Vedra, Jethro Joe A.")
+print("Vedra, Jethro Joe A.")
+print("Vedra, Jethro Joe A.")
+print("Vedra, Jethro Joe A.")
+print("Vedra, Jethro Joe A.")
+print("Vedra, Jethro Joe A.")
+
+
+
+print("ITCS_102 Computer Programming")
+print("ITCS_102 Computer Programming")
+print("ITCS_102 Computer Programming")
+print("ITCS_102 Computer Programming")
+print("ITCS_102 Computer Programming")
+print("ITCS_102 Computer Programming")
+print("ITCS_102 Computer Programming")
+print("ITCS_102 Computer Programming")
+print("ITCS_102 Computer Programming")
+print("ITCS_102 Computer Programming")
+
