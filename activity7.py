0c92ca2e621c58ab584067aa223008ec5a575c28
vjethrojoe-cyber <vjethrojoe@gmail.com>
2026-09-07 00:17:37 +0800

    Create activity7.py

---
+++ activity7.py
@@ -0,0 +1,35 @@
+total = 1000
+
+x = eval(input("Enter number of x --> "))
+total -= x
+
+y = eval(input("Enter number of y --> "))
+total -= y
+
+z = eval(input("Enter number of z --> "))
+total -= z
+
+a = eval(input("Enter number of a --> "))
+total -= a
+
+b = eval(input("Enter number of b --> "))
+total -= b
+
+c = eval(input("Enter number of c --> "))
+total -= c
+
+d = eval(input("Enter number of d --> "))
+total -= d
+
+e = eval(input("Enter number of e --> "))
+total -= e
+
+f = eval(input("Enter number of f --> "))
+total -= f
+
+g = eval(input("Enter number of g --> "))
+total -= g
+
+
+
+print("Remaining Total is -->", total)
