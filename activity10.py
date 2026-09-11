97b1c7b923a2910fd04319beb76fa752315639d2
vjethrojoe-cyber <vjethrojoe@gmail.com>
2026-09-11 20:31:23 +0800

    Create activity10.py

---
+++ activity10.py
@@ -0,0 +1,11 @@
+username = "NeverGrowOld"
+password = "pwede_magtanong?"
+
+u = input("Input USERNAME ---> ")
+p = input("Input PASSWORD ---> ")
+
+if u == username and p == password:
+    print("Access Granted")
+
+else:
+    print("Access Denied")
